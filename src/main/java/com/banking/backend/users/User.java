308e812a55d7ac334b63e3ac3dfdaa62320ec68b
LKBotond <l.kovacs.botond@gmail.com>
2025-08-25 23:00:32 +0300

    set up the user object

---
+++ src/main/java/com/banking/backend/users/User.java
@@ -0,0 +1,91 @@
+package com.banking.backend.users;
+
+import java.util.ArrayList;
+
+import com.banking.backend.accounts.Account;
+
+//redesign a bit, tldr, do not need the whole thingy, just the specifics, meaning separate register and login dao and stuff
+
+public class User {
+    private int userID;
+    private String email;
+    private String encryptedName;
+    private String IV;
+    private String name;
+    private String familyName;
+    private String passHash;
+    private ArrayList<Account> accounts;
+
+    public User(int user_ID, String email, String encryptedName, String IV) {
+        this.userID = user_ID;
+        this.email = email;
+        this.encryptedName = encryptedName;
+        this.IV = IV;
+
+    }
+
+    public static User empty() {
+        return new User(0, null, null, null);
+    }
+
+    public void setAccountsForUser(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void addNewAccountToUser(Account account) {
+        this.accounts.add(account);
+    }
+
+    public Account getAccountByID(int accountID) {
+        for (Account account : accounts) {
+            if (account.getAccounID() == accountID) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Account> getAllAccounts() {
+        return accounts;
+    }
+
+    public void splitName(String fullName) {
+        String[] parts = fullName.split(" ", 2);
+        this.name = parts[0];
+        if (parts.length > 1) {
+            this.familyName = parts[1];
+        } else {
+            this.familyName = "";
+        }
+
+    }
+
+    public String getEncryptedName() {
+        return this.encryptedName;
+    }
+
+    public String getIV() {
+        return this.IV;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getFamilyName() {
+        return this.familyName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getUserId() {
+        return this.userID;
+    }
+
+    public String getPassHash() {
+        return this.passHash;
+    }
+
+}
