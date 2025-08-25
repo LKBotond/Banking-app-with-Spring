30f9923af5b7252870a4518f57f4f7a1d7dfb38b
LKBotond <l.kovacs.botond@gmail.com>
2025-08-25 22:57:57 +0300

    set up a class that holds the SQL queries as public static final Strings

---
+++ src/main/java/com/banking/backend/dbAccess/DBQueries.java
@@ -0,0 +1,22 @@
+package com.banking.backend.dbAccess;
+
+public final class DBQueries {
+    private DBQueries() {}
+    public static final String CREATE_ACCOUNT = SQLFileReader.readSQL("SQL/Business/accounts/create_account.sql");
+    public static final String CREATE_USER = SQLFileReader.readSQL("SQL/Business/users/create_user.sql");
+    
+    public static final String GET_PASS_HASH=SQLFileReader.readSQL("SQL/Business/users/get_pass_hash.sql");
+    public static final String GET_USER=SQLFileReader.readSQL("SQL/Business/users/get_user.sql");
+
+    public static final String GET_ACCOUNT_IDS=SQLFileReader.readSQL("SQL/Business/accounts/get_account_ids.sql");
+    public static final String GET_ACCOUNT_BY_ID=SQLFileReader.readSQL("SQL/Business/accounts/get_account_by_id.sql");
+    public static final String GET_FUNDS_FOR_ACCOUNT=SQLFileReader.readSQL("SQL/Business/accounts/get_funds_for_account.sql");
+    public static final String UPDATE_FUNDS_FOR_ACCOUNT_ID=SQLFileReader.readSQL("SQL/Business/accounts/update_funds.sql");
+    
+    public static final String LOGIN=SQLFileReader.readSQL("SQL/Business/logins/login.sql");
+    public static final String LOGOUT=SQLFileReader.readSQL("SQL/Business/logins/logout.sql");
+
+    public static final String GET_RECEIVER_DATA=SQLFileReader.readSQL("SQL/Business/master_record/get_receiver_data.sql");
+    public static final String GET_SENDER_DATA=SQLFileReader.readSQL("SQL/Business/master_record/get_sender_data.sql");
+    public static final String GET_TRANSACTIONS_BETWEEN_TWO_ACCOUNTS=SQLFileReader.readSQL("SQL/Business/master_record/get_transactions_between_two_accounts.sql");
+}
