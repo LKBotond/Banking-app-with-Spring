ca6202d05f9ad35b3ae1580ca57d3bac4ee5e126
LKBotond <l.kovacs.botond@gmail.com>
2025-09-18 17:18:37 +0300

    Added DTO's for the future controllers

--- src/main/java/com/banking/backend/controllers/AuthenticationControllers.java
+++ src/main/java/com/banking/backend/controllers/AuthenticationControllers.java
@@ -1,8 +1,12 @@
 package com.banking.backend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController 
 public class AuthenticationControllers {
-    
+
 }
