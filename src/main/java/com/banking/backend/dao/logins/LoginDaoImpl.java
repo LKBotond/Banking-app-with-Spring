1b193052977ecec124ab4805592a0c92f9868a0c
LKBotond <l.kovacs.botond@gmail.com>
2025-10-21 11:40:42 +0300

    added: /dao/logins/loginDao.java: interface for querying the logins table. /dao/logins/loginDaoImpl.java: implementation for the interface.

---
+++ src/main/java/com/banking/backend/dao/logins/LoginDaoImpl.java
@@ -0,0 +1,35 @@
+package com.banking.backend.dao.logins;
+
+import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.banking.backend.Legacy.dbAccess.DBQueries;
+
+@Repository
+public class LoginDaoImpl implements LoginDao {
+
+    private final JdbcTemplate jddJdbcTemplate;
+
+    public LoginDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jddJdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<Long> login(long userID) {
+        try {
+            Long rs = jddJdbcTemplate.queryForObject(DBQueries.LOGIN, Long.class, userID);
+            return Optional.of(rs);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void logout(long loginId) {
+        jddJdbcTemplate.update(DBQueries.LOGOUT, loginId);
+    }
+
+}
